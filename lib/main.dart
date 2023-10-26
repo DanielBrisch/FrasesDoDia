83eb4bf86f7d126987f2c07e7c32971481369543
DanielBrisch <danielbrisch157@gmail.com>
2023-10-26 00:39:58 -0300

    estudos dias 24/01, fazendo app de frases do dia sozinho, me desafiando

--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,75 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
-  runApp( MaterialApp(
+  runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     title: "Frases do dia",
-    home: _HomeStateful(),
+    home: HomeStateful(),
   ));
 }
 
-class _HomeStateful extends StatefulWidget {
-  const _HomeStateful({super.key});
-
-  @override
-  State<_HomeStateful> createState() => _HomeStatefulStateState();
+T getItemAleatorio<T>(List<T> lista) {
+  var rng = Random();
+  return lista[rng.nextInt(lista.length)];
 }
 
-class _HomeStatefulStateState extends State<_HomeStateful> {
+final List<String> frases = [
+    "Frase 1",
+    "Frase 2",
+    "Frase 3",
+    "Frase 4",
+];
+
+String fraseAtual = "Seja Bem Vindo(a)!!";
+
+class HomeStateful extends StatefulWidget {
+  const HomeStateful({Key? key}) : super(key: key);
+
+  @override
+  _HomeStatefulState createState() => _HomeStatefulState();
+}
+
+class _HomeStatefulState extends State<HomeStateful> {
+  var _texto = "PRIMEIRO";
+
   @override
   Widget build(BuildContext context) {
-    var _titulo = "NuBank";
     return Scaffold(
       appBar: AppBar(
-        title: Text(_titulo),
-        backgroundColor: Colors.deepPurpleAccent,
+        title: Text("Frases do Dia"),
+        backgroundColor: Colors.green,
       ),
-      body: Padding(
-          padding: EdgeInsets.all(16),
-          child: Text("Conteudo Teste")
-      ),
-      bottomNavigationBar: BottomAppBar(
-        color: Colors.deepPurpleAccent,
-        child: Padding(
-            padding: EdgeInsets.all(16),
-            child: Row(
-              children: [
-                Text("TESTE "),
-                Text("TESTE "),
-                Text("TESTE "),
-              ],
-            )
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Padding(
+                padding: EdgeInsets.only(top: 50),
+                child: Image.asset('assets/img/logo.png')), // Image
+            SizedBox(height: 40),
+            Center(
+              child: Container(
+                width: 140,
+                height: 40,
+                child: ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      getItemAleatorio(frases);
+                    });
+                  },
+                  child: Text("Clique aqui"),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.black26,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 40),
+            Text("Nome: $_texto"),
+          ],
         ),
       ),
-    );;
-  }
-}
-
-
-class Home extends StatelessWidget {
-
-  @override
-  Widget build(BuildContext context) {
-    return;
+    );
   }
 }
